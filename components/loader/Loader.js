00d16b3b856c99c64032a4f05f9dbed6f98084dd
Rutuj2804 <bokaderutuj36@gmail.com>
2023-03-25 19:19:56 +0530

    loader improved

--- components/loader/Loader.js
+++ components/loader/Loader.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useRef } from "react";
-import Lottie from "lottie-web";
-import animationData from "../../assets/data/Paperplane.json"
+import React from "react";
+import { CircularProgress } from "@mui/material";
 
 const Loader = () => {
 
-    const container = useRef()
-
-    useEffect(()=>{
-        Lottie.loadAnimation({
-            container: container.current,
-            renderer: "svg",
-            loop: true,
-            autoplay: true,
-            animationData: animationData
-        })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
 	return <div className="loader">
-        <div className="central" ref={container}></div>
+    <CircularProgress color="secondary" />
     </div>;
 };
 
